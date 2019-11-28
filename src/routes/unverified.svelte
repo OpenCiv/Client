8eef958f6a0490378b25d035631729c35d138abc
Maarten Lensink <m.lensink@symagic.com>
2019-11-28 14:12:40 +0200

    Lower case filenames

---
+++ src/routes/unverified.svelte
@@ -0,0 +1,7 @@
+<script>
+function resend() {
+
+}
+</script>
+<p>The account has not yet been verified.</p>
+<p>Click <button on:click={resend} class="link">here</button> to resend a verification e-mail.</p>

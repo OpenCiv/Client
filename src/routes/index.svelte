e110c95bea44f42f4e0bb70927e55036fee477fa
Maarten Lensink <m.lensink@symagic.com>
2019-11-30 23:15:22 +0200

    This map comes from straland.com!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { alerts } from '../stores.js';
 
 onMount(() => {
+   alerts.update(a => [...a, 'Hello, world!']);
    axios.get('checkin.php').then(response => {
       if (response.data) {
          sapper.goto('game');
@@ -12,6 +13,7 @@ onMount(() => {
          sapper.goto('login');
       }
    }).catch(error => {
+      alerts.update(a => error ? error.message || error : 'unknown error');
       console.log(error ? error.message || error : 'unknown error');
    });
 });

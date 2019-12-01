2275238261931b2d36461141cc8e5b027fc22d04
Maarten Lensink <m.lensink@symagic.com>
2019-12-02 01:35:58 +0200

    Stop propagation

--- src/components/Unit.svelte
+++ src/components/Unit.svelte
@@ -3,11 +3,12 @@ import { selected } from '../stores.js';
 
 export let unit;
 
-function select() {
+function select(e) {
+   e.stopPropagation();
    selected.update(s => unit);
 }
 </script>
 
 <div>
-   <img src="img/u_nordic.png" alt="unit" on:click={e => select()}>
+   <img src="img/u_nordic.png" alt="unit" on:click={e => select(e)}>
 </div>

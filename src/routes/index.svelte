a81be833f7a1d816b405887167154130cde37c5a
Maarten Lensink <m.lensink@symagic.com>
2019-12-04 13:19:23 +0200

    Login, menu or game

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,14 +6,17 @@ import { alerts } from '../stores.js';
 
 onMount(() => {
    axios.get('checkin.php').then(response => {
-      if (response.data) {
+      if (response.data > 0) {
+         sapper.goto('game/' + response.data);
+      }
+      else if (response.data === 0) {
          sapper.goto('menu');
       } else {
          // sapper.goto('login');
          sapper.goto('game/1');
       }
    }).catch(error => {
-      alerts.update(a => error ? error.message || error : 'unknown error');
+      alerts.set(error ? error.message || error : 'unknown error');
       console.log(error ? error.message || error : 'unknown error');
    });
 });

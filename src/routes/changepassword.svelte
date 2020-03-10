fd0eb4f36d0e610004c283f59c2fc7f64975239a
Jeke <jerkko.valta@gmail.com>
2020-03-10 21:26:42 +0200

    Fonts update
    Added link for webfont file. Changed a few H2 to H1. A page should always have one H1.

--- src/routes/changepassword.svelte
+++ src/routes/changepassword.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="https://fonts.googleapis.com/css?family=Hind:400,600|Lora:400i&display=swap" rel="stylesheet">
+</svelte:head>
+
 <style lang="less">
    @import url("../less/layout.less");
 </style>

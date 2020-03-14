4059b114066f6b26a99c7e89fe3b4c740056ed44
Maarten Lensink <m.lensink@symagic.com>
2020-03-14 16:31:40 +0200

    Master back-end

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import * as sapper from '@sapper/app';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://openciv.eu/dev/';
+axios.defaults.baseURL = 'https://openciv.eu/php/';
 sapper.start({
 	target: document.querySelector('#sapper')
 });

c2130ffe2f18a7e0c44f35d745f0a78d9fa588d3
Maarten Lensink <m.lensink@symagic.com>
2020-03-27 19:58:30 +0200

    Images in subdirectories

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -14,14 +14,14 @@
 </style>
 
 <script>
-import { backend, selected, player } from '../stores';
+import { alerts, backend, selected, player } from '../stores';
 
 export let mapdata;
 
 export let mapsize;
 
 function img_src(category, type) {
-   return `img/${category.charAt(0)}_${type}.png`;
+   return `img/${category}/${type}.png`;
 }
 
 function resource_quantity(resource) {
@@ -44,37 +44,7 @@ async function tile_click(e, tile) {
    }
 
    const action = await backend('move', { id: $selected.id, x: tile.x, y: tile.y });
-   if (!action) {
-      return;
-   }
-
-   $selected.action = action;
-
-   /*
-   const old = mapdata[$selected.y][$selected.x]['units'].indexOf($selected);
-   if (old === -1) {
-      alerts.add('Error moving unit');
-   }
-
-   mapdata[$selected.y][$selected.x]['units'].splice(old, 1);
-   $selected.x = tile.x;
-   $selected.y = tile.y;
-   tile.units.push($selected);
-   mapdata = mapdata;
-   */
-}
-
-/**
- * Returns whether the selected units is allowed to move to the tile.
- */
-function can_move(tile) {
-   if (tile.type === 'water' || !$selected) {
-      return false;
-   }
-
-   let x = Math.abs(tile.x - $selected.x);
-   let y = Math.abs(tile.y - $selected.y);
-   return x < 2 && y < 2 && x + y > 0;
+   alerts.add(JSON.stringify(action));
 }
 </script>
 
@@ -83,27 +53,26 @@ function can_move(tile) {
       {#each mapdata as row}
          <div class="map_row" style="width: {mapsize.x * 128}px;">
             {#each row as tile}
-               <!-- <div class="tile {tile.type === 'water' ? 'tile_ocean' : 'tile_plains'}" class:canmove={can_move(tile)} on:mousedown={e => tile_click(e, tile)}> -->
                <div class="tile {tile.type === 'water' ? 'tile_ocean' : 'tile_plains'}" on:mousedown={e => tile_click(e, tile)}>
                   {#each tile.improvements as improvement}
                      <div class="improvement">
-                        <img src={img_src('building', improvement.type)} alt={improvement.type}>
+                        <img src="img/improvements/{improvement.type}.png" alt={improvement.type}>
                      </div>
                   {/each}
                   {#each tile.units as unit}
                      <div class="unit">
                         <div class="player-banner">
                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 150" preserveAspectRatio="none">
-                              <!-- The fill of this polygon should be adjusted to be players primary color. -->
+                              <!-- The fill of this polygon should be adjusted to be player's primary color. -->
                               <polygon fill="#F2D346" points="0,0 100,0 100,100 50,120 0,100"/>
 
-                              <!-- The fill of this polygon should be adjusted to be players secondary color. -->
+                              <!-- The fill of this polygon should be adjusted to be player's secondary color. -->
                               <polygon fill="#FFEC96" points="0,0 100,0 100,10 0,10"/>
 
                               <!-- To be added: player custom icon, also with secondary color -->
                            </svg>
                         </div>
-                        <img src={img_src('unit', 'nordic')} alt={'nordic'} class:active={unit === $selected}>
+                        <img src="img/units/nordic.png" alt="nordic" class:active={unit === $selected}>
                      </div>
                   {/each}
                   {#each tile.resources as resource}
@@ -112,7 +81,7 @@ function can_move(tile) {
                            <polygon fill="#777777" points="0,0 64,0 64,64 0,64"/>
                            <circle cx="50%" cy="50%" r="75" stroke="#777777" stroke-width="8" fill="#444444" />
                         </svg>
-                        <img src={img_src('resource', resource.type)} alt={resource_quantity(resource)}>
+                        <img src="img/resources/{resource.type}.svg" alt={resource_quantity(resource)}>
                      </div>
                   {/each}
                </div>

8158e1d223739f03956df8aa860f7eee8a038fb4
Jeke <jerkko.valta@gmail.com>
2020-04-15 21:37:57 +0300

    Header wealth indicator, research indicator, other stuff
    Adjusted unit size on map. Added header stuff such as wealth. Stopped using improvement z-index.

--- src/routes/game/[id]/index.svelte
+++ src/routes/game/[id]/index.svelte
@@ -125,7 +125,11 @@ async function endTurn() {
       </p>
    </div>
    <div id="research-bar" class="fourth">
-      <p class="center"><span class="research">&sext; Researching</span> {researchBarResearch}</p>
+      <p class="center">
+         <img src="img/resources/wealth.svg" class="tiny-icon" alt="Wealth">
+         <span class="wealth">123 </span>
+         <img src="img/resources/science.svg" class="tiny-icon" alt="Science">
+         <span class="research">+123 &rArr;</span> {researchBarResearch} (17)</p>
    </div>
    <div id="time-bar" class="fourth">
       <p class="center">

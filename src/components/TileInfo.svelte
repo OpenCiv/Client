7c1961c30778e410b2bd645457ff7e894f6c9b8a
Maarten Lensink <m.lensink@symagic.com>
2020-04-22 20:43:36 +0300

    Tile info

--- src/components/TileInfo.svelte
+++ src/components/TileInfo.svelte
@@ -4,8 +4,18 @@
 
 <script>
 import { hoveredTile } from '../stores';
+import { capitalize, Colors } from '../utilities';
 </script>
 
 <div id="tileinfo">
-{$hoveredTile.type}
+<span>{capitalize($hoveredTile.type)}</span>
+{#if $hoveredTile.vegetation}
+<br><span>{capitalize($hoveredTile.vegetation)}</span>
+{/if}
+{#if $hoveredTile.improvement}
+<br><span>{capitalize($hoveredTile.improvement.type)}</span>
+{/if}
+{#each $hoveredTile.units as unit}
+<br><span style="color: {Colors.getPrimaryColor(unit.color)}">Unit</span>
+{/each}
 </div>

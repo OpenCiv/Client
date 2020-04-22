0e5efc7cbe61c453092e474065a4b88b8b410ae3
Maarten Lensink <m.lensink@symagic.com>
2020-04-22 22:18:24 +0300

    Show improvement percentage

--- src/components/TileInfo.svelte
+++ src/components/TileInfo.svelte
@@ -16,7 +16,7 @@ import { capitalize, Colors } from '../utilities';
       <br><span>{capitalize(resource.type)} ({resource.quantity})</span>
    {/each}
    {#if $hoveredTile.improvement}
-      <br><span>{capitalize($hoveredTile.improvement.type)}</span>
+      <br><span>{capitalize($hoveredTile.improvement.type)}{#if $hoveredTile.improvement.completion < 1}&nbsp;({Math.round(100 * $hoveredTile.improvement.completion, 0)}%){/if}</span>
    {/if}
    {#each $hoveredTile.units as unit}
       <br><span style="color: {Colors.getUnitPrimaryColor(unit)}">Unit{#if unit.actions && unit.actions.length > 0}&nbsp;({unit.actions[0].type}){/if}</span>

6cc9c625dff9b7681ce62777d9f5792e5e36e198
Maarten Lensink <m.lensink@symagic.com>
2020-08-05 22:26:04 +0300

    Tile information

--- src/components/TileInfo.svelte
+++ src/components/TileInfo.svelte
@@ -18,16 +18,23 @@ import { capitalize, Colors } from '../utilities';
    <tbody>
       <tr>
          <td class="hide-mobile right"><h4>Terrain</h4></td>
-         <td><span>{capitalize($hoveredTile.type)}</span></td>
+         <td>
+            <span>
+               {capitalize($hoveredTile.type)}{#if $hoveredTile.hill}, hills{/if}
+            </span>
+         </td>
       </tr>
       <tr>
          <td class="hide-mobile right"><h4>Features</h4></td>
          <td>
             {#if $hoveredTile.improvement}
-               <span>{capitalize($hoveredTile.improvement.type)}{#if $hoveredTile.improvement.completion < 1}&nbsp;({Math.round(100 * $hoveredTile.improvement.completion, 0)}%){/if},</span>
+               <span>{capitalize($hoveredTile.improvement.type)}{#if $hoveredTile.improvement.completion < 1}
+                     ({Math.round(100 * $hoveredTile.improvement.completion, 0)}%)
+                  {/if}{#if $hoveredTile.vegetation},{/if}
+               </span>
             {/if}
             {#if $hoveredTile.vegetation}
-               <span>{capitalize($hoveredTile.vegetation)}</span>
+               <span>{$hoveredTile.improvement ? $hoveredTile.vegetation : capitalize($hoveredTile.vegetation)}</span>
             {/if}
          </td>
       </tr>
@@ -44,7 +51,11 @@ import { capitalize, Colors } from '../utilities';
          <td class="hide-mobile right"><h4>Units</h4></td>
          <td>
             {#each $hoveredTile.units as unit}
-               <span style="color: {Colors.getUnitPrimaryColor(unit)}">Unit{#if unit.actions && unit.actions.length > 0}&nbsp;({unit.actions[0].type}){/if}</span>
+               <span style="color: {Colors.getUnitPrimaryColor(unit)}">Unit
+                  {#if unit.actions && unit.actions.length > 0}
+                     ({unit.actions[0].type})
+                  {/if}
+               </span>
             {/each}
          </td>
       </tr>
